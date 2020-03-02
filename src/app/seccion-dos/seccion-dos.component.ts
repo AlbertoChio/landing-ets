b3f883ec9377dc95b48f1183d17d6f6a7793b483
AlbertoChio <a.chio@outlook.com>
2020-03-01 23:55:20 -0600

    beneficios

--- src/app/seccion-dos/seccion-dos.component.ts
+++ src/app/seccion-dos/seccion-dos.component.ts
@@ -22,9 +22,145 @@ export class SeccionDosComponent implements OnInit {
   practicodescripciontxt="Edith Capacita te garantiza un curso 80% de practica y 20% de teoría."
   medidadescripciontxt="En Edith Capacita practicaras técnicas de aprendizaje acelerado a través de ejercicios individuales y grupales, asi cmomo dinámicas de role playing."
   instructordescripciontxt="Instructor experto en liderazgo, capacitado a niveles directivos, gerenciales, así como jefes de área y supervisores en las organizaciones. Hasta el momento más de 2,500 horas impartidas en el tema."
-  constructor() { }
+
+  comunicacionblanco:string="assets/seccion-dos/comunicacionblanco.png"
+  conocerasblanco:string="assets/seccion-dos/conocerasblanco.png"
+  desarrolloblanco:string="assets/seccion-dos/desarrolloblanco.png"
+  efectivoblanco:string="assets/seccion-dos/efectivoblanco.png"
+  modelosblanco:string="assets/seccion-dos/modelosblanco.png"
+  valoresblanco:string="assets/seccion-dos/valoresblanco.png"
+  estiloblanco:string="assets/seccion-dos/estiloblanco.jpg"
+  beneficios:string="BENEFICIOS DEL CURSO"
+
+  comunicacionrojo:string="assets/seccion-dos/comunicacionrojo.png"
+  conocerasrojo:string="assets/seccion-dos/conocerasrojo.png"
+  desarrollorojo:string="assets/seccion-dos/desarrollorojo.png"
+  efectivorojo:string="assets/seccion-dos/efectivorojo.png"
+  modelosrojo:string="assets/seccion-dos/modelosrojo.png"
+  valoresrojo:string="assets/seccion-dos/valoresrojo.png"
+  estilorojo:string="assets/seccion-dos/estilorojo.png"
+
+  selectedb:string="comunicacion"
+  public beneficiosobj:any[] =[
+    {
+      "beneficio": "comunicacion",
+      "titulo":"COMUNICACIÓN EFECTIVA",
+      "descripciones":[
+        {
+          "id": 1,
+          "descripcion":"Practicará la comunicación efectiva para comunicarce a todos los nivieles de la organización y para saber dar ordenes e instrucciones a sus colaboradores."
+        },
+        {
+          "id": 2,
+          "descripcion":"Practicará la motivación positiva a través del reconocimiento, adecuado a su mensaje a cada individuo y sus motivaciones."
+        },
+        {
+          "id": 3,
+          "descripcion":"Practicará la retroalimentación positiva y negativa de acuerdo con los modelos de comunicación asertiva."
+        }
+      ],
+      "ico":{
+        "rojo":"assets/seccion-dos/comunicacionrojo.png",
+        "blanco":"assets/seccion-dos/comunicacionblanco.png"
+      }
+    },
+    {
+      "beneficio": "desarrollo",
+      "titulo":"DESARROLLO DE EQUIPOS",
+      "descripciones":[
+        {
+          "id": 1,
+          "descripcion":"Practicará las estrategías que le permitirán integrar y desarrollar equipos de trabajo efectivos, por medio de la implementación de códigos de equipo."
+        }
+      ],
+      "ico":{
+        "rojo":"assets/seccion-dos/desarrollorojo.png",
+        "blanco":"assets/seccion-dos/desarrolloblanco.png"
+      }
+    },
+    {
+      "beneficio": "liderazgo",
+      "titulo":"LIDERAZGO EFECTIVO O INEFECTIVO",
+      "descripciones":[
+        {
+          "id": 1,
+          "descripcion":"Identificará si su liderazgo es efectivo o inefectivo, y cuales son las causas."
+        }
+      ],
+      "ico":{
+        "rojo":"assets/seccion-dos/efectivorojo.png",
+        "blanco":"assets/seccion-dos/efectivoblanco.png"
+      }
+    },
+    {
+      "beneficio": "valores",
+      "titulo":"LIDERAZGO CON VALORES Y PRINCIPIOS",
+      "descripciones":[
+        {
+          "id": 1,
+          "descripcion":"Identificará las variables para aplicar el liderazgo correcto con acuerdo a la teoría de liderazgo situacional."
+        }
+      ],
+      "ico":{
+        "rojo":"assets/seccion-dos/valoresrojo.png",
+        "blanco":"assets/seccion-dos/valoresblanco.png"
+      }
+    },
+    {
+      "beneficio": "estilo",
+      "titulo":"DESCUBRE TU ESTILO DE LIDERAZGO",
+      "descripciones":[
+        {
+          "id": 1,
+          "descripcion":"Descubre tu estilo de liderazgo prodominante y su origen por medio de un test mismo que se analizará de manera personalizada."
+        },
+        {
+          "id": 2,
+          "descripcion":"Aprenderá las conductas y actitudes de cada estilo de liderazgo, así como sus ventajas y desventajas."
+        }
+      ],
+      "ico":{
+        "rojo":"assets/seccion-dos/estilorojo.png",
+        "blanco":"assets/seccion-dos/estiloblanco.png"
+      }
+    },
+    {
+      "beneficio": "modelos",
+      "titulo":"MODELOS DE COACHING",
+      "descripciones":[
+        {
+          "id": 1,
+          "descripcion":"Practicará los modelos de coaching para resolver los problemas típicos de desempeño y de malas conductas o actitudes de su equipo de trabajo y en lo individual."
+        },
+        {
+          "id": 2,
+          "descripcion":"Practicará técnicas de negociación para conceder, en lugar de ceder."
+        },
+        {
+          "id": 3,
+          "descripcion":"Reconocerá la inteligencia emocional como una herramienta prioritaria en la toma de decisiones y generadora de relaciones armónicas."
+        }
+      ],
+      "ico":{
+        "rojo":"assets/seccion-dos/modelosrojo.png",
+        "blanco":"assets/seccion-dos/modelosblanco.png"
+      }
+    }
+
+  ]
 
   ngOnInit(): void {
   }
+  banderarombof(beneficio:string){
+    this.selectedb=beneficio;
+  }
 
+
+  filterItemsOfType(beneficio){
+    let descripciones:any[]=[];
+    let beneficiof=this.beneficiosobj.filter(x => x.beneficio == beneficio);
+
+    return beneficiof;
+
+  }
 }
